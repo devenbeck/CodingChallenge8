d49dca2d9892139c28d6e085562161069c1321f4
Natalie Devenbeck <devenbeck@usf.edu>
2025-02-16 22:31:11 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -6,3 +6,12 @@ function calculateSalary(baseSalary, bonus, taxRate){
 }
 console.log(calculateSalary(5000, 500, 0.1)); // should show net salary of 5000
 console.log(calculateSalary(7000, 1000, 0.15)); //should show net salary of 6950
+
+//Task 2: Function Expression
+//Scenario: Product Price After Discount
+function calculateDiscount(price, discountRate) {
+    let finalPrice = price - (price * discountRate); //formula given
+    return `Final Price: $${finalPrice.toFixed(2)}`; //making the result to 2 decimal points
+}
+console.log(calculateDiscount(100, 0.2)); // Price should be $80.00
+console.log(calculateDiscount(250, 0.15)); // Price should be $212.50
